63c327588b2e485b84a9cfefc87a8e7e1bc3d8fa
성수연 <brrenda20051@gmail.com>
2026-03-19 18:19:04 +0900

    파일경로 옮기기

--- example/android/app/src/main/kotlin/kr/ac/daelim/example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package kr.ac.daelim.example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- example/dart/intro.dart
+++
@@ -1,4 +0,0 @@
-void main() {
-  int score = 0;
-  var list = <dynamic>[1, 2, 3, 'four', 'five'];
-}
